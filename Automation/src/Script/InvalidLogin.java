f4efa6e2754faff604cc7bbb6545c1a121ec76bc
syed76hassan <37832759+syed76hassan@users.noreply.github.com>
2018-03-28 09:26:34 +0530

    latest

--- Automation/src/Script/InvalidLogin.java
+++ Automation/src/Script/InvalidLogin.java
@@ -1,5 +1,23 @@
 package Script;
 
-public class InvalidLogin {
+import org.testng.annotations.Test;
+
+import Generic.BaseTest;
+import Generic.Excel;
+import Page.LoginPage;
+
+public class InvalidLogin extends BaseTest {
+
+	@Test(priority=1,groups= {"Login","Smoke"})
+	public void TestInValidLogin()
+	{
+		String un = Excel.getValue(XL_PATH, "invalid", 1, 0);
+		String ps=Excel.getValue(XL_PATH, "invalid", 1, 1);
+		
+		LoginPage l=new LoginPage(driver);
+		l.SetUserName(un);
+		l.SetPassword(ps);
+		l.ClickLogin();
+	}
 
 }
